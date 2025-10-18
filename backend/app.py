c519853ce0c1f47d5cf5b7aeeaaa7bf4c6f54cd6
Ishanth <sooramishanth@gmail.com>
2025-10-18 11:22:16 -0600

    Socket fixes

--- backend/app.py
+++ backend/app.py
@@ -1024,4 +1024,5 @@ def handle_help_request(data):
     emit('help_response', {'message': 'Redirecting to My Notes...'})
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, host='0.0.0.0', port=port, debug=False)
